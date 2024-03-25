001518792e11fbafd7a89817870e3911d5ac3076
Necfol <necfoll@gmail.com>
2024-03-25 22:47:56 +0800

    fix(add google api): add google api

--- app/api/grammer/route.ts
+++ app/api/grammer/route.ts
@@ -1,9 +1,27 @@
+/*
+ * @LastEditors: Necfol
+ * @Date: 2024-03-25 22:37:06
+ * @LastEditTime: 2024-03-25 22:47:42
+ * @FilePath: /Blog-Template-Pro/app/api/grammer/route.ts
+ */
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-  const json = {
-    message: "Hello World"
-  };
+export async function POST(req: Request) {
+  const params = await req.json()
+  const res = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${params.key}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      contents: [{
+        parts: [{
+          text: `帮我检查一下这段话的语法，并且给出建议：${params.text}`
+        }]
+      }]
+    })
+  })
 
-  return NextResponse.json(json);
+  const data = await res.json()
+  return NextResponse.json(data)
 }
